087382c06e436b85030044149314be37612e2cf9
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-06-16 15:52:34 +0545

    add event controller with create, update, and delete event functions

---
+++ server/controllers/event.controller.js
@@ -0,0 +1,13 @@
+import { StatusCodes } from 'http-status-codes';
+
+export const createEvent = async (req, res) => {
+  
+};
+
+export const updateEvent = async (req, res) => {
+    
+  };
+
+export const deleteEvent = async (req, res) => {
+
+  };
