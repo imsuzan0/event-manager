2e6d4c3343cdadaba4e7c4ac4055269faf4c7728
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-06-16 19:47:41 +0545

    fix: correct syntax and improve error handling in database connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
+    console.log(`MongoDB Connected:${conn.connection.host}`);
+  } catch (err) {
+    console.error("Error in db connection", err);
+    process.exit(1);
+  }
+};
 
-const connectDB = async()=>{
-    try{
-        mongoose.connect(MONGO_URI)
-    }catch(err){
-        console.error(err)
-        throw err
-    }
-}
+export default connectDB;
